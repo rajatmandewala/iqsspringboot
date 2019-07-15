4e280067fd66877f78856f3b22ae0d002738741b
rajatmandewala <rajatmandewala@gmail.com>
2019-07-15 12:09:52 +0530

    after updating database library1

--- src/main/java/com/iqsspringboot/controller/UserController.java
+++ src/main/java/com/iqsspringboot/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.iqsspringboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class UserController {	
+	
+	@GetMapping("/")
 	public String demo(){		
 		return "asd";
 	}
