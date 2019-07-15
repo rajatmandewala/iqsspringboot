d86757decd33524ad42a50238161f7b4b566d47e
rajatmandewala <rajatmandewala@gmail.com>
2019-07-15 12:13:15 +0530

    1

--- src/main/java/com/iqsspringboot/controller/UserController.java
+++ src/main/java/com/iqsspringboot/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.iqsspringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {	
+@RequestMapping("/user")
+public class UserController {
 	
-	@GetMapping("/")
+	@RequestMapping("/rajat")
 	public String demo(){		
 		return "asd";
 	}
